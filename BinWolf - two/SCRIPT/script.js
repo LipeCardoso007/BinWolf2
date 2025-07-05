fb9d323a455c74c15be516e14777fe3c59545ecb
LipeCardoso007 <felipe11gc@gmail.com>
2025-07-05 14:34:11 -0400

    Delete BinWolf - two directory

--- BinWolf - two/SCRIPT/script.js
+++
@@ -1,9 +0,0 @@
-function trocarbutao() {
-
-    const menu = document.getElementById("submenuheader");
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    }
-}
